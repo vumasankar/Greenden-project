d46efa7ce7e5813713f48b2992afc394825e0940
vumasankar <vishvaja2k@gmail.com>
2024-02-16 17:06:21 +0530

    search bar added

--- product.js
+++ product.js
@@ -10,5 +10,27 @@ closenav.addEventListener("click",function(){
     sidenav.style.right = "-50%"
 })
 
+var productContainer = document.getElementById("product-container")
 
+var search = document.getElementById("search")
+
+var productList = productContainer.querySelectorAll("div")
+
+
+
+search.addEventListener("keyup",function(){
+    var enteredValue = event.target.value.toUpperCase()
+    for(count =0; count<productList.length;count=count+1){
+        var productName = productList[count].querySelector("h1").textContent
+
+        if(productName.toUpperCase().indexOf(enteredValue)<0){
+
+            productList[count].style.display ="none"
+
+        }
+        else{
+            productList[count].style.display ="block"
+        }
+    }
+})
 
